dc2bf7a13e4bc801a7179400962a80dd1afe8eb5
Lin Sam <sam.lin@tripsaas.com>
2019-04-29 23:46:43 +0800

    fix load

--- Load.py
+++ Load.py
@@ -49,21 +49,30 @@ def LoadData(table, select, date, article_type = ''):
         except:
             conn.close()
             return ''
-    def load(table ,date, select, article_type):
+    def load(table ,date, select, article_type = ''):
         if isinstance(select,list):
             select2 = "`,`".join(select)
         else:
             select = [select]
-        select2 = select
+            select2 = "','".join(select)
+            
+        if isinstance(article_type,list):
+            article_type2 = "','".join(article_type)
+        else:
+            article_type = [article_type]
+            article_type2 = "','".join(article_type)
+        #article_type2 = article_type
+        #select2 = select
         
-        sql = "select `{}` from {} WHERE `date` >= '{}'".format(select2,table,date)
-        if article_type != '': sql = "{} AND `article_type` = '{}' ".format(sql,article_type)
+        sql = "select `{}` from `{}` WHERE `date` >= '{}'".format(select2,table,date)
+        if article_type2 != '': 
+            sql = "{} AND `article_type` IN ('{}') ".format(sql,article_type2)
         tem = execute_sql2(sql)
         data = pd.DataFrame(list(tem))
         if len(data) > 0:
             data.columns = select
         return data
-    
+
     #-----------------------------------------------
     if isinstance(select,str) or isinstance(select,list):
         data = load(table ,date, select, article_type)
